f9729823c30b114faf65f92b884f79b36c0764ab
LiliIsabel <per20051@byui.edu>
2022-09-17 17:55:48 -0600

    2ndcommmit

--- scripts/date.js
+++ scripts/date.js
@@ -1,1 +1,5 @@
-document.querySelector('#lastUpdated').textContent = 'Last Updated: ${document.lastModified}';
+
+document.querySelector('#lastUpdated').textContent = `Last Updated: ${document.lastModified}`;
+const d = new Date();
+let year = d.getFullYear();
+document.querySelector('#year').textContent = `\u00a9 ${year} Lili Perez | Herriman, UT`;
