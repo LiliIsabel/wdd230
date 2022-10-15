fc9f797601db35c06c445d5148014ab2d97e25b5
LiliIsabel <per20051@byui.edu>
2022-10-14 22:19:31 -0600

    heroimage

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,6 +1,7 @@
 
 document.querySelector('#lastUpdated').textContent = `Last Updated: ${document.lastModified}`;
 const d = new Date();
+currentDate = d.getDay();
 let year = d.getFullYear();
 document.querySelector('#year').textContent = `\u00a9 ${year} Mountain Valley Chamber | Lili Perez | WDD 230 Project |`;
 let date = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format(d);
@@ -14,3 +15,9 @@ x.onclick = toggleMenu;
 
 const banner =document.querySelector('#date').textContent =` ${date} ` ;
 
+
+if(currentDate == 1 || currentDate == 4){
+    document.querySelector('#meeting').textContent =` "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."` ;
+} else{
+    document.querySelector('#meeting').style.display="none";
+}
