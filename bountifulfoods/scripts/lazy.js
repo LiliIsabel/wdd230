a5075d2a0ae448fc20bdac5e4763124f1c3a354e
LiliIsabel <per20051@byui.edu>
2022-12-15 02:57:50 -0700

    lazy loading

---
+++ bountifulfoods/scripts/lazy.js
@@ -0,0 +1,32 @@
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imageOptions = {
+    threshold: 1,
+    rootMargin:  '0px 0px 50px 0px',   
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+if ('IntersectionObserver' in window) {
+    const imageObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach(item => {
+                if (item.isIntersecting) {
+                    loadImages(item.target);
+                    imageObserver.unobserve(item.target);
+                }
+        });
+    }, imageOptions);
+
+
+imagesToLoad.forEach(img => {
+    imageObserver.observe(img);
+});
+} else {
+    imagesToLoad.forEach(img => {
+        loadImages(img);
+    });
+}
