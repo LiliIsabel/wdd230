dfd1099747c468a4aa5e5690549f37a12767451a
LiliIsabel <per20051@byui.edu>
2022-12-14 22:31:05 -0700

    about

--- bountifulfoods/scripts/scripts.js
+++ bountifulfoods/scripts/scripts.js
@@ -3,7 +3,7 @@ document.querySelector('#lastUpdated').textContent = `Last Updated: ${document.l
 const d = new Date();
 currentDate = d.getDay();
 let year = d.getFullYear();
-document.querySelector('#year').textContent = `\u00a9 ${year} Bountiful Foods| Lili Perez | WDD 230 Project |`;
+document.querySelector('#year').textContent = `\u00a9 ${year} Bountiful Foods | Lili Perez | WDD 230 Project |`;
 let date = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format(d);
 
 function toggleMenu(){
